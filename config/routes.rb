bf800d92e0268c3a1514036f9d8dd405a2c80499
Mario Chois <mario.chois.navarro@gmail.com>
2021-11-18 15:54:13 -0500

    Database: Added seed to users table in database

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  
+  get 'groups/show', to: "groups#show", as: "groups_show"
+  get 'groups/new', to: "groups#new", as: "groups_new"
+  get 'entities/show', to: "entities#show", as: "entities_show"
+  get 'entities/new', to: "entities#new", as: "entities_new"
   devise_for :users
   root to: "recipes#show"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
