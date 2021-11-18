0c3a347792428d7865b8d31b34bdceeeac5ef658
Mario Chois <mario.chois.navarro@gmail.com>
2021-11-18 16:30:50 -0500

    Views: Added splash screen view with exception to authenticate user

--- app/models/entity.rb
+++ app/models/entity.rb
@@ -1,4 +1,4 @@
-class Recipe < ApplicationRecord
+class Entity < ApplicationRecord
   belongs_to :user, class_name: 'User'
   has_many :group_entities, dependent: :destroy
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
+  get 'splash/show', to: "splash#show", as: "splash_show"
   get 'groups/show', to: "groups#show", as: "groups_show"
   get 'groups/new', to: "groups#new", as: "groups_new"
   get 'entities/show', to: "entities#show", as: "entities_show"
   get 'entities/new', to: "entities#new", as: "entities_new"
   devise_for :users
-  root to: "recipes#show"
+  root to: "splash#show"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
