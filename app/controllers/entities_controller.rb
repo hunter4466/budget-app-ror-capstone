a6c68bfe6cc49715f6ecb1f273308e8e2646a71f
Mario Chois <mario.chois.navarro@gmail.com>
2021-11-18 23:18:30 -0500

    Bug fix: fixed input type to save a new Transaction, now is able to input a floating point number

--- app/controllers/entities_controller.rb
+++ app/controllers/entities_controller.rb
@@ -3,9 +3,12 @@ class EntitiesController < ApplicationController
         @cat_id = params[:cat_id]
         @group_entities = GroupEntity.where(group_id: params[:cat_id]).order(:created_at)
         @entities = []
+        sum = 0
         @group_entities.each do |e|
             @entities << Entity.where(id: e.entity_id)
+            sum += Entity.where(id: e.entity_id)[0].amount.to_f
         end
+        @sumatory = sum
     end
 
     def new
@@ -19,15 +22,19 @@ class EntitiesController < ApplicationController
         @categories = Group.where(user_id: current_user.id)
         p 'did this'
         count = 0
+        matcher = 0
+        gruopidtosend = false
         @categories.each do |e|
             if params[:entity][e.name.parameterize.underscore.to_sym].to_i > 0
                 count += 1
+                if matcher == 0
+                    gruopidtosend = e.id
+                end
             end
         end
         if count == 0
-            p 'didnt pass the test'
-            flash.now[:error] = 'Record could not be created'
             redirect_to entities_new_url(prev_data: params[:entity][:name], prev_data_two: params[:entity][:amount])
+            flash.now[:error] = 'Record could not be created'
         else
             @entity = Entity.new
             @entity.name = params[:entity][:name]
@@ -42,6 +49,8 @@ class EntitiesController < ApplicationController
                     @group_entity.save
                 end
             end
+            redirect_to entities_show_url(cat_id: gruopidtosend)
+            flash.now[:notice] = 'Transaction has been succesfully saved'
         end
     end
 end
