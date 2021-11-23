3ba217ac5184e5271d56b4a53adaca6252f47477
Mario Chois <mario.chois.navarro@gmail.com>
2021-11-23 18:55:51 -0500

    Fix: User now doesn't need email validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :confirmable
+         :recoverable, :rememberable, :validatable
   has_many :entities, foreign_key: 'user_id', class_name: 'Entity'
   has_many :groups, foreign_key: 'user_id', class_name: 'Group'
 end
